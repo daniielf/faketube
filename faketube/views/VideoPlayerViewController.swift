18a25132aa21b9972083e3ac1ce531c64fcc627e
Daniel Freitas <danielf@daniels-air.br.ibm.com>
2019-09-23 15:13:16 -0300

    Video Player Controller Video Controlling

--- faketube/views/VideoPlayerViewController.swift
+++ faketube/views/VideoPlayerViewController.swift
@@ -25,6 +25,11 @@ class VideoPlayerViewController: UIViewController {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var playButton: UIImageView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    
+    @IBOutlet weak var durationTimeLabel: UILabel!
+    
+    @IBOutlet weak var seeker: UISlider!
     
     var video: Video?
     var player: AVPlayer?
@@ -65,13 +70,20 @@ class VideoPlayerViewController: UIViewController {
         player = AVPlayer(url: videoURL!)
         let playerLayer = AVPlayerLayer(player: player)
         
-        videoScreenView!.layer.addSublayer(playerLayer)
+
+        
+        videoScreenView.layer.addSublayer(playerLayer)
         playerLayer.frame = CGRect(x: 0, y: 0, width: videoScreenView.frame.width, height: videoScreenView.frame.height)
         
         player?.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.old, .new], context: nil)
         player?.volume = 0
         player?.play()
         
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor(white: 0, alpha: 1).cgColor]
+        gradientLayer.locations = [0.8, 1.1]
+        gradientLayer.frame = videoScreenView.bounds
+        videoScreenView.layer.addSublayer(gradientLayer)
     }
     
     func setupVideoControl() {
@@ -80,8 +92,39 @@ class VideoPlayerViewController: UIViewController {
         videoScreenView.bringSubview(toFront: controlView)
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(switchVideoPlayerStatus))
-        
         controlView.addGestureRecognizer(tapGesture)
+        
+        seeker.value = 0
+        seeker.addTarget(self, action: #selector(updateSeekerAndVideoTimer), for: .valueChanged)
+        
+        
+        let interval = CMTime(value: 1, timescale: 2)
+        player?.addPeriodicTimeObserver(forInterval: interval, queue: .main, using: { (time) in
+            
+            var timeS = time.seconds
+            let timeMinutes = Int(timeS / 60)
+            let timeSeconds = Int(timeS) % 60
+
+            self.currentTimeLabel.text = String(format: "%.2d:%.2d", timeMinutes, timeSeconds)
+            
+            if let duration = self.player?.currentItem?.duration {
+                let durationSeconds = duration.seconds
+                self.seeker.setValue(Float(timeS / durationSeconds), animated: true)
+            }
+        })
+        
+    }
+    
+    @objc func updateSeekerAndVideoTimer() {
+        let travelPercent = seeker.value
+        
+        if let time = self.player?.currentItem?.duration.seconds {
+            let travelToTime = time * Double(travelPercent)
+            let cgTime = CMTime(seconds: travelToTime, preferredTimescale: 1)
+            player?.seek(to: cgTime)
+        }
+        
+        
     }
     
     @objc func switchVideoPlayerStatus() {
@@ -104,7 +147,15 @@ class VideoPlayerViewController: UIViewController {
                 activityIndicator.isHidden = true
                 setupVideoControl()
                 videoScreenView.bringSubview(toFront: controlView)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                    let timeSeconds = CMTimeGetSeconds((self.player?.currentItem?.duration)!)
+                    
+                    let durationSeconds = Int(timeSeconds) % 60
+                    let durationMinutes = Int(timeSeconds / 60)
 
+                    self.durationTimeLabel.text = String(format: "%.2d:%.2d", durationMinutes, durationSeconds)
+                })
             default: break
             }
         }
